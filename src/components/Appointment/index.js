d18f7decb59a7513997827c6a1eb9aa696f1e216
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2020-10-25 20:19:34 -0400

    Update index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -65,7 +65,7 @@ const Appointment = ({ appointments }) => {
   return (
     <>
       <div className="hero-container">
-        <h2>DIGITRON BOOKING APP</h1>
+        <h2>DIGITRON BOOKING APP</h2>
       </div>
       <div className="AppointmentWrapper">
         <div className="navWrapper">
